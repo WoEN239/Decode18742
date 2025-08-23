e3cc8b68ee48207dac7524135554ee954cbcf8de
tikhonsmovzh <81905988+tikhonsmovzh@users.noreply.github.com>
2025-08-23 20:11:12 +0300

    feat(ThreadingTelemetry): многопоточные конфиги

--- TeamCode/src/main/kotlin/org/woen/telemetry/ThreadedTelemetry.kt
+++ TeamCode/src/main/kotlin/org/woen/telemetry/ThreadedTelemetry.kt
@@ -4,26 +4,44 @@ import android.content.Context
 import com.acmerobotics.dashboard.FtcDashboard
 import com.acmerobotics.dashboard.config.ValueProvider
 import org.firstinspires.ftc.ftccommon.external.OnCreate
+import java.util.concurrent.atomic.AtomicReference
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.findAnnotation
+
+annotation class ThreadedConfig(val category: String, val name: String = "")
 
 class ThreadedTelemetry {
-    companion object{
+    companion object {
         @OnCreate
         @JvmStatic
         fun create(context: Context?) {
             FtcDashboard.start(context)
 
-            FtcDashboard.getInstance().addConfigVariable<Int>("a", "b", SimpleProvider(5))
+            val configs = ThreadedConfigs::class.declaredMemberProperties.filter {
+                it.returnType.classifier == AtomicProvider::class &&
+                        it.findAnnotation<ThreadedConfig>() != null
+            }
+
+            for (i in configs) {
+                val value = i.get(ThreadedConfigs) as AtomicProvider<*>
+                val annotation = i.annotations[0] as ThreadedConfig
+
+                FtcDashboard.getInstance().addConfigVariable(
+                    annotation.category,
+                    annotation.name.ifEmpty { i.name }, value
+                )
+            }
         }
     }
 
-    class SimpleProvider<T>(default: T): ValueProvider<T>{
-        private var data = default
+    class AtomicProvider<T>(default: T) : ValueProvider<T> {
+        private var data = AtomicReference(default)
 
-        override fun get() = data
+        override fun get(): T = data.get()
 
         override fun set(value: T?) {
-            if(value != null)
-                data = value
+            if (value != null)
+                data.set(value)
         }
     }
 }
