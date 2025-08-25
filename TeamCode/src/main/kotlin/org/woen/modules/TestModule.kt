3f90d95bd8dc4fd1aad825116eb915046d3a03cc
tikhonsmovzh <81905988+tikhonsmovzh@users.noreply.github.com>
2025-08-25 22:02:06 +0300

    feat(ThreadingTelemetry): временная отправка в телеметрию

--- TeamCode/src/main/kotlin/org/woen/modules/TestModule.kt
+++ TeamCode/src/main/kotlin/org/woen/modules/TestModule.kt
@@ -1,13 +1,18 @@
 package org.woen.modules
 
 import org.woen.telemetry.ThreadedTelemetry
+import org.woen.utils.timers.ReversedElapsedTime
 
 data class A(var a: Int = 5)
 
-class TestModule: IModule<A> {
+class TestModule : IModule<A> {
     init {
-        ThreadedTelemetry.LAZY_INSTANCE.addTelemetrySender {
-            it.addData("b", 6)
+        ThreadedTelemetry.LAZY_INSTANCE.startTemporarySender(ReversedElapsedTime(10.0), {
+            it.addLine("b")
+        })
+
+        ThreadedTelemetry.LAZY_INSTANCE.addSender {
+            it.addData("c", 4)
         }
     }
 
---
+++ TeamCode/src/main/kotlin/org/woen/utils/timers/ReversedElapsedTime.kt
@@ -0,0 +1,21 @@
+package org.woen.utils.timers
+
+class ReversedElapsedTime(startTimeSeconds: Double = 0.0): ElapsedTimeExtra() {
+    private var _startTimeNanos = (startTimeSeconds * SECOND_IN_NANO).toLong()
+
+    fun resetWithStartSeconds(time: Double){
+        _startTimeNanos = (time * SECOND_IN_NANO).toLong()
+    }
+
+    fun resetWithStartNanos(time: Long){
+        _startTimeNanos = time
+    }
+
+    fun resetWithStartMillis(time: Double){
+        _startTimeNanos = (time * MILLIS_IN_NANO).toLong()
+    }
+
+    override fun nanoseconds(): Long {
+        return _startTimeNanos - super.nanoseconds()
+    }
+}
