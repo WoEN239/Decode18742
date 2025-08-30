e14d10585cdafd84c47544dcea34097fcfcef4e3
tikhonsmovzh <81905988+tikhonsmovzh@users.noreply.github.com>
2025-08-30 22:57:36 +0300

    feat(GamepadOpMode): callback для обновления геймпада

--- TeamCode/src/main/kotlin/org/woen/linearOpModes/TeleOpMode.kt
+++ TeamCode/src/main/kotlin/org/woen/linearOpModes/TeleOpMode.kt
@@ -1,13 +1,13 @@
 package org.woen.linearOpModes
 
+import com.qualcomm.robotcore.eventloop.opmode.GamepadOpMode
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import org.firstinspires.ftc.robotcore.external.Telemetry
 import org.woen.hotRun.HotRun
 
 @TeleOp
-class TeleOpMode: LinearOpMode() {
-
+class TeleOpMode: GamepadOpMode() {
     override fun runOpMode() {
         if(HotRun.INSTANCE == null)
             HotRun.init()
