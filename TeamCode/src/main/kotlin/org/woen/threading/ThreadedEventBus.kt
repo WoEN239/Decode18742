50d371030a292c93047b4db6a936bd92a54ffe5b
tikhonsmovzh <81905988+tikhonsmovzh@users.noreply.github.com>
2025-09-10 18:08:30 +0300

    fix(documentation): исправлена документация

--- TeamCode/src/main/kotlin/org/woen/threading/ThreadedEventBus.kt
+++ TeamCode/src/main/kotlin/org/woen/threading/ThreadedEventBus.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
+import org.woen.threading.hardware.HardwareThreads
 import kotlin.reflect.KClass
 
 @Suppress("UNCHECKED_CAST")
