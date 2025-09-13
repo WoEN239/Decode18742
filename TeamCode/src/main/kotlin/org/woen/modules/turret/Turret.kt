6b4eb988b606c050ab036020f13e67b82a3529ab
tikhonsmovzh <81905988+tikhonsmovzh@users.noreply.github.com>
2025-09-13 18:43:35 +0300

    feat(RequireBallEvent): евент запроса

--- TeamCode/src/main/kotlin/org/woen/modules/turret/Turret.kt
+++ TeamCode/src/main/kotlin/org/woen/modules/turret/Turret.kt
@@ -1,16 +1,43 @@
 package org.woen.modules.turret
 
+import barrel.enumerators.BallRequest
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import org.woen.modules.IModule
 import org.woen.threading.ThreadManager
+import org.woen.threading.ThreadedEventBus
 import org.woen.threading.hardware.HardwareThreads
+import org.woen.utils.process.Process
 
-class Turret: IModule {
+data class TurretShootEvent(
+    val ball: BallRequest.Name,
+    var isSuccessful: Boolean = false,
+    val process: Process = Process()
+)
+
+data class TurretShootPatternEvent(
+    val pattern: Pattern,
+    var isSuccessful: Boolean = false,
+    val process: Process = Process()
+)
+
+data class TurretShootAllBarrelEvent(val process: Process = Process())
+
+data class RequireBallEvent(val ball: BallRequest.Name, val process: Process = Process())
+
+class Turret : IModule {
     private val _hardwareTurret = HardwareTurret("turretMotor")
 
     init {
         HardwareThreads.LAZY_INSTANCE.EXPANSION.addDevices(_hardwareTurret)
+
+        ThreadedEventBus.LAZY_INSTANCE.subscribe(TurretShootEvent::class, {
+
+        })
+
+        ThreadedEventBus.LAZY_INSTANCE.subscribe(TurretShootPatternEvent::class, {
+            
+        })
     }
 
     private var _turretJob: Job? = null
