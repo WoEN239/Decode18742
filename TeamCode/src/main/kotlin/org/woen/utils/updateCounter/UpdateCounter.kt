2988f4b3a97c9feec1707069a761d9d9db0e5b78
tikhonsmovzh <81905988+tikhonsmovzh@users.noreply.github.com>
2025-09-21 12:30:41 +0300

    fix: исправление UpdateCounter

--- TeamCode/src/main/kotlin/org/woen/utils/updateCounter/UpdateCounter.kt
+++ TeamCode/src/main/kotlin/org/woen/utils/updateCounter/UpdateCounter.kt
@@ -1,16 +1,19 @@
 package org.woen.utils.updateCounter
 
 import com.qualcomm.robotcore.util.ElapsedTime
+import java.util.concurrent.atomic.AtomicReference
 
 class UpdateCounter {
     private val _updateTimer = ElapsedTime()
 
-    var currentUPS = 0.0 // update per second
-        private set
+    val currentUPS: Double
+        get() = _atomicCurrentUPS.get()
+
+    private val _atomicCurrentUPS = AtomicReference(0.0)
 
     @Synchronized
     fun update() {
-        currentUPS = 1.0 / _updateTimer.seconds()
+        _atomicCurrentUPS.set(1.0 / _updateTimer.seconds())
 
         _updateTimer.reset()
     }
