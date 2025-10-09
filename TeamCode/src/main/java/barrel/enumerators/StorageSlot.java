38c05c9a502ea078b74dc5c4b10ac975a7b2229f
Gyro <gyroscopic.why@gmail.com>
2025-10-09 22:34:17 +0300

    Refactor Slot & Ball enums for new storage system

--- TeamCode/src/main/java/barrel/enumerators/StorageSlot.java
+++ TeamCode/src/main/java/barrel/enumerators/StorageSlot.java
@@ -3,78 +3,96 @@ package barrel.enumerators;
 
 public class StorageSlot
 {
-    public enum Name
+    static public final int BOTTOM = 0, CENTER = 1, MOBILE_OUT = 2, MOBILE_IN = 3, MOBILE = 4, UNDEFINED = 5;
+
+    private Name _memName;
+    private int _memId;
+
+
+    StorageSlot(int value)
     {
-        LEFT,
-        CENTER,
-        RIGHT
+        Set(ToName(value), value);
+    }
+    StorageSlot(Name name)
+    {
+        Set(name, ToInt(name));
+    }
+    StorageSlot(int value, Name name)
+    {
+        Set(name, value);
+    }
+    StorageSlot(Name name, int value)
+    {
+        Set(name, value);
+    }
+    public StorageSlot()
+    {
+        Set(UNDEFINED, Name.UNDEFINED);
     }
 
 
-    Name memName;
-    int memId;
+    public enum Name
+    {
+        BOTTOM,
+        CENTER,
+        MOBILE_OUT,
+        MOBILE_IN,
+        MOBILE,
+        UNDEFINED
+    }
 
 
     public void Set(int value)
     {
-        memId = value;
-        memName = ToName(value);
+        Set(ToName(value), value);
     }
     public void Set(Name name)
     {
-        memId = ToInt(name);
-        memName = name;
+        Set(name, ToInt(name));
     }
     public void Set(int value, Name name)
     {
-        memId = value;
-        memName = name;
+        Set(name, value);
     }
     public void Set(Name name, int value)
     {
-        memId = value;
-        memName = name;
+        _memId = value;
+        _memName = name;
     }
 
 
     public Name GetName()
     {
-        return memName;
+        return _memName;
     }
     public int GetId()
     {
-        return memId;
-    }
-
-
-
-    static public int LEFT()
-    {
-        return 0;
-    }
-    static public int CENTER()
-    {
-        return 1;
-    }
-    static public int RIGHT()
-    {
-        return 2;
+        return _memId;
     }
 
 
     static public Name ToName (int value)
     {
-        return value == 0 ? Name.LEFT
-                : value == 1 ? Name.CENTER
-                : Name.RIGHT;
+        switch (value)
+        {
+            case BOTTOM:     return Name.BOTTOM;
+            case CENTER:     return Name.CENTER;
+            case MOBILE_OUT: return Name.MOBILE_OUT;
+            case MOBILE_IN:  return Name.MOBILE_IN;
+            case MOBILE:     return Name.MOBILE;
+            default:  return Name.UNDEFINED;
+        }
     }
     static public int ToInt (Name name)
     {
         switch (name)
         {
-            case LEFT:   return 0;
-            case CENTER: return 1;
-            default:     return 2;
+            case BOTTOM: return BOTTOM;
+            case CENTER: return CENTER;
+            case MOBILE_OUT: return MOBILE_OUT;
+            case MOBILE_IN:  return MOBILE_IN;
+            case MOBILE:     return MOBILE;
+            default:     return UNDEFINED;
         }
     }
 }
---
+++ TeamCode/src/main/kotlin/org/woen/modules/storage/MobileSlot.java
@@ -0,0 +1,46 @@
+package org.woen.modules.storage;
+
+import barrel.enumerators.Ball;
+import barrel.enumerators.StorageSlot;
+
+public class MobileSlot
+{
+    private StorageSlot _slot = new StorageSlot();
+    private Ball _ball = new Ball();
+
+
+
+    public StorageSlot Slot()
+    {
+        return _slot;
+    }
+    public int SlotId()
+    {
+        return _slot.GetId();
+    }
+    public StorageSlot.Name SlotName()
+    {
+        return _slot.GetName();
+    }
+
+
+
+    public Ball Ball()
+    {
+        return _ball;
+    }
+    public int BallId()
+    {
+        return _ball.GetId();
+    }
+    public Ball.Name BallName()
+    {
+        return _ball.GetName();
+    }
+
+
+    public int Count()
+    {
+        return _ball.GetId() == 0 ? 0 : 1;
+    }
+}
