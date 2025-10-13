e385b7dfcac38aaa18a469334481ecddd3134d74
Gyro <gyroscopic.why@gmail.com>
2025-10-13 23:57:31 +0300

    +StorageCells: Intake & Request handling, ball count feedback

---
+++ TeamCode/src/main/kotlin/org/woen/modules/storage/Storage.kt
@@ -0,0 +1,15 @@
+package org.woen.modules.storage
+
+import barrel.enumerators.Ball
+
+class Storage
+{
+    private var storageCells: Array<Ball>;
+
+    init {
+        storageCells = arrayOf(Ball(), Ball(), Ball(), Ball())
+    }
+
+
+
+}
