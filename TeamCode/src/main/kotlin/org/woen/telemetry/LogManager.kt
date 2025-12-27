c83ab1d8c4d9907d058c3ddb6df007beefa9cb72
Gyro <gyroscopic.why@gmail.com>
2025-12-27 19:13:23 +0300

    +LogManager util

---
+++ TeamCode/src/main/kotlin/org/woen/telemetry/LogManager.kt
@@ -0,0 +1,105 @@
+package org.woen.telemetry
+
+
+
+class LogManager
+{
+    private var _showLevels = arrayListOf(0u)
+    private var _debugShowSetting = DebugSetting.SHOW_ABOVE_SELECTED_INCLUSIVE
+    private var _moduleName = ""
+
+
+    constructor(showSetting: DebugSetting = DebugSetting.SHOW_ABOVE_SELECTED_INCLUSIVE,
+        debugShowSettings: ArrayList<UInt> = arrayListOf(0u),
+        moduleName: String = "")
+    {
+        updateDebugSetting(showSetting)
+        setShowedDebugLevels(debugShowSettings)
+        setModuleName(moduleName)
+    }
+
+
+    enum class DebugSetting
+    {
+        SHOW_SELECTED_LEVELS,
+
+        SHOW_ABOVE_SELECTED_INCLUSIVE,
+        SHOW_ABOVE_SELECTED_EXCLUSIVE,
+
+        SHOW_BELOW_SELECTED_INCLUSIVE,
+        SHOW_BELOW_SELECTED_EXCLUSIVE
+    }
+
+
+
+    fun updateDebugSetting(showSetting: DebugSetting)
+    {
+        _debugShowSetting = showSetting
+    }
+    fun setShowedDebugLevels(showLevels: ArrayList<UInt>)
+    {
+        _showLevels = ArrayList(showLevels)
+    }
+    fun setModuleName(moduleName: String)
+    {
+        _moduleName = moduleName
+    }
+
+
+
+    fun log   (s: String, debugLevel: UInt)
+    {
+        if (allowedToShow(debugLevel))
+            ThreadedTelemetry.LAZY_INSTANCE.log(s)
+    }
+    fun logMd (s: String, debugLevel: UInt)
+    {
+        if (allowedToShow(debugLevel))
+            ThreadedTelemetry.LAZY_INSTANCE.log(toMdString(s))
+    }
+    fun logTag(s: String, tag: String, debugLevel: UInt)
+    {
+        if (allowedToShow(debugLevel))
+            ThreadedTelemetry.LAZY_INSTANCE.logWithTag(s, tag)
+    }
+
+
+
+    private fun toMdString(s: String)
+        = if (_moduleName.isEmpty()) s
+          else "$_moduleName: $s"
+    private fun allowedToShow(debugLevel: UInt): Boolean
+    {
+        return when (_debugShowSetting)
+        {
+            DebugSetting.SHOW_SELECTED_LEVELS -> customSelected(debugLevel)
+
+            DebugSetting.SHOW_ABOVE_SELECTED_INCLUSIVE -> aboveInclusive(debugLevel)
+            DebugSetting.SHOW_ABOVE_SELECTED_EXCLUSIVE -> aboveExclusive(debugLevel)
+
+            DebugSetting.SHOW_BELOW_SELECTED_INCLUSIVE -> belowInclusive(debugLevel)
+            DebugSetting.SHOW_BELOW_SELECTED_EXCLUSIVE -> belowExclusive(debugLevel)
+        }
+    }
+
+
+
+    private fun customSelected(debugLevel: UInt): Boolean
+            = _showLevels.contains(debugLevel)
+
+
+    private fun aboveInclusive(debugLevel: UInt): Boolean
+        = if (_showLevels.isEmpty()) false
+          else debugLevel >= _showLevels[0]
+    private fun aboveExclusive(debugLevel: UInt): Boolean
+        = if (_showLevels.isEmpty()) false
+          else debugLevel >  _showLevels[0]
+
+
+    private fun belowInclusive(debugLevel: UInt): Boolean
+        = if (_showLevels.isEmpty()) false
+          else debugLevel <= _showLevels[0]
+    private fun belowExclusive(debugLevel: UInt): Boolean
+        = if (_showLevels.isEmpty()) false
+          else debugLevel <  _showLevels[0]
+}
