7b0b99302206a5522e3b5e50c330b0b0bc0491ab
Gyro <gyroscopic.why@gmail.com>
2026-01-10 23:15:32 +0000

    Fixed: RunStatus.kt crashing when spamming calls of a .remove / .indexOf dependent method

--- TeamCode/src/main/kotlin/org/woen/utils/process/RunStatus.kt
+++ TeamCode/src/main/kotlin/org/woen/utils/process/RunStatus.kt
@@ -51,47 +51,22 @@ class RunStatus
     }
 
 
+
     fun isNotBusy()          =  _processQueue.isEmpty()
     fun isUsedByAnyProcess() = !_processQueue.isEmpty()
-    fun isUsedByThisProcess(targetProcessId: Int): Boolean
-    {
-        if (_processQueue.isEmpty()) return false
+    fun isUsedByThisProcess(targetProcessId: Int)
+        = _processQueue.contains(targetProcessId)
+    fun isUsedByAnotherProcess(exceptionProcessId: Int)
+        = _processQueue.any { it != exceptionProcessId }
 
-        for (curProcess in _processQueue)
-            if (curProcess == targetProcessId) return true
-
-        return false
-    }
-    fun isUsedByAnotherProcess(exceptionProcessId: Int): Boolean
-    {
-        if (_processQueue.isEmpty()) return false
-
-        for (curProcess in _processQueue)
-            if (curProcess != exceptionProcessId) return true
-
-        return false
-    }
 
 
     fun addProcessToQueue(processId: Int) = _processQueue.add(processId)
     fun clearAllProcesses() = _processQueue.clear()
     fun safeRemoveThisProcessIdFromQueue(processId: Int)
-    {
-        if (_processQueue.isEmpty()) return
-
-        try {
-            do {
-                val position = _processQueue.indexOf(processId)
-                if (position != -1) _processQueue.removeAt(position)
-            } while (position != -1)
-        }
-        finally { }
-    }
+        = _processQueue.removeAll { it == processId }
     fun safeRemoveOnlyOneInstanceOfThisProcessFromQueue(processId: Int)
-    {
-        val position  =     _processQueue.indexOf(processId)
-        if (position != -1) _processQueue.removeAt(position)
-    }
+        = _processQueue.remove(processId)
 
 
 
@@ -171,23 +146,10 @@ class RunStatus
 
 
     fun isForcedToTerminateThisProcess(processId: Int) = _terminationList.contains(processId)
-    fun addProcessToTerminationList(processId: Int)
-    {
-        safeRemoveThisProcessIdFromQueue(processId)
-        _terminationList.add(processId)
-    }
-    fun safeRemoveThisProcessFromTerminationList(processId: Int)
-    {
-        if (_terminationList.isEmpty()) return
+    fun addProcessToTerminationList   (processId: Int) = _terminationList.add(processId)
 
-        try {
-            do {
-                val position = _terminationList.indexOf(processId)
-                if (position != -1) _terminationList.removeAt(position)
-            } while (position != -1)
-        }
-        finally { }
-    }
+    fun safeRemoveThisProcessFromTerminationList(processId: Int)
+        = _terminationList.removeAll { it == processId }
 
     fun clearAllTermination() = _terminationList.clear()
 
