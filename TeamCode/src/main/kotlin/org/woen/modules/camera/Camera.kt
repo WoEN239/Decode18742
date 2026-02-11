df72ee7a4d02e94e6c8a0eb3b49271ab285a6f7b
Koksic <konstantinzelenkov9@gmail.com>
2026-02-11 16:58:19 +0300

    add Limelight Camera.kt And TurretLocalizer.kt

--- TeamCode/src/main/kotlin/org/woen/modules/camera/Camera.kt
+++ TeamCode/src/main/kotlin/org/woen/modules/camera/Camera.kt
@@ -1,163 +1,108 @@
-/*package org.woen.modules.camera
+package org.woen.modules.camera
 
-import com.acmerobotics.dashboard.FtcDashboard
+import com.qualcomm.hardware.limelightvision.LLResult
 import com.qualcomm.hardware.limelightvision.Limelight3A
 import com.qualcomm.robotcore.eventloop.opmode.OpModeManagerImpl
 import kotlinx.coroutines.DisposableHandle
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D
 import org.firstinspires.ftc.robotcore.internal.system.AppUtil
-import org.firstinspires.ftc.vision.VisionPortal
-import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor
 import org.woen.hotRun.HotRun
 import org.woen.modules.scoringSystem.turret.Pattern
-import org.woen.telemetry.configs.Configs
+import org.woen.telemetry.Configs
 import org.woen.telemetry.ThreadedTelemetry
 import org.woen.threading.ThreadManager
 import org.woen.threading.ThreadedEventBus
 import org.woen.utils.smartMutex.SmartMutex
 import kotlin.concurrent.thread
 
-
+// Твои события
 data class OnPatternDetectedEvent(val pattern: Pattern)
 data class GetRobotCoords(val x: Double, val z: Double)
-class CloseCameraEvent()
 
-private var limelight: Limelight3A? = null
-private val localizer = TurretLocalizer()
-class Camera : DisposableHandle {
+class CameraLL : DisposableHandle {
     companion object {
-        private var _nullableInstance: Camera? = null
-
+        private var _nullableInstance: CameraLL? = null
         private val _instanceMutex = SmartMutex()
 
         @JvmStatic
-        val LAZY_INSTANCE: Camera
+        val LAZY_INSTANCE: CameraLL
             get() = _instanceMutex.smartLock {
                 if (_nullableInstance == null)
-                    _nullableInstance = Camera()
-
+                    _nullableInstance = CameraLL()
                 return@smartLock _nullableInstance!!
             }
-
-        fun restart() {
-            _instanceMutex.smartLock {
-                _nullableInstance?.dispose()
-                _nullableInstance = null
-            }
-        }
     }
 
-    private var _visionPortal: VisionPortal? = null
+    private val hardwareMap = OpModeManagerImpl.getOpModeManagerOfActivity(AppUtil.getInstance().activity).hardwareMap
+    private val limelight = hardwareMap.get(Limelight3A::class.java, "limelight")
+    private val localizer = TurretLocalizer()
 
-    private var _aprilProcessor: AprilTagProcessor? = null
+    @Volatile
+    var turretAngle: Double = 0.0
+
+    @Volatile
+    var robotPose: Pose3D? = null
+
+    // Геттеры для удобного доступа
+    val robotX: Double get() = robotPose?.position?.x ?: 0.0
+    val robotZ: Double get() = robotPose?.position?.y ?: 0.0 // Используем y из Pose3D как вторую координату поля
 
     var currentPattern: Pattern? = null
 
     private val _thread =
-        ThreadManager.LAZY_INSTANCE.register(thread(start = true, name = "Camera thread") {
+        ThreadManager.LAZY_INSTANCE.register(thread(start = true, name = "Limelight thread") {
+            limelight.pipelineSwitch(0)
+            limelight.start()
+
             while (!Thread.currentThread().isInterrupted && Configs.CAMERA.CAMERA_ENABLE) {
-                if (HotRun.LAZY_INSTANCE.currentRunState != HotRun.RunState.RUN ||
-                    _aprilProcessor == null ||
-                    _visionPortal?.cameraState != VisionPortal.CameraState.STREAMING
-                ) {
-                    if (_visionPortal?.cameraState == VisionPortal.CameraState.ERROR) {
-                        reCreateCamera()
-                        ThreadedTelemetry.LAZY_INSTANCE.log("camera restarted cause: err")
+                if (HotRun.LAZY_INSTANCE.currentRunState != HotRun.RunState.RUN) {
+                    Thread.sleep(20)
+                    continue
+                }
+
+                val result: LLResult? = limelight.latestResult
+
+                if (result != null && result.isValid) {
+                    // 1. Расчет локализации
+                    val newPose = localizer.getGlobalRobotPose(limelight, turretAngle)
+
+                    if (newPose != null) {
+                        robotPose = newPose
+                        // Генерируем событие с координатами
+                        ThreadedEventBus.LAZY_INSTANCE.invoke(
+                            GetRobotCoords(robotX, robotZ)
+                        )
                     }
 
-                    Thread.sleep(5)
-                    continue
-                }
+                    // 2. Логика паттернов
+                    val fiducialResults = result.fiducialResults
+                    if (currentPattern == null && fiducialResults.isNotEmpty()) {
+                        for (fr in fiducialResults) {
+                            val tagId = fr.fiducialId
+                            val foundPattern = Pattern.patterns.find { it.cameraTagId == tagId.toInt() }
 
-                val detections = _aprilProcessor?.freshDetections
-
-                if (detections == null) {
-                    Thread.sleep(5)
-                    continue
-                }
-
-                if (currentPattern == null)
-                    for (i in detections) {
-                        currentPattern = Pattern.patterns.find { it.cameraTagId == i.id }
-
-                        if (currentPattern != null) {
-                            ThreadedEventBus.LAZY_INSTANCE.invoke(
-                                OnPatternDetectedEvent(
-                                    currentPattern!!
-                                )
-                            )
-
-                            ThreadedTelemetry.LAZY_INSTANCE.log("motiv detected: " +
-                                    "${currentPattern!!.subsequence[0]} ${currentPattern!!.subsequence[1]} ${currentPattern!!.subsequence[2]}")
+                            if (foundPattern != null) {
+                                currentPattern = foundPattern
+                                ThreadedEventBus.LAZY_INSTANCE.invoke(OnPatternDetectedEvent(currentPattern!!))
+                                ThreadedTelemetry.LAZY_INSTANCE.log("LL Pattern: ${foundPattern.cameraTagId}")
+                            }
                         }
                     }
+                }
+                Thread.sleep(10)
             }
         })
 
-    fun reCreateCamera() {
-        if (_visionPortal?.cameraState == VisionPortal.CameraState.STREAMING) {
-            FtcDashboard.getInstance().stopCameraStream()
-            _visionPortal?.close()
-
-            while (_visionPortal?.cameraState != VisionPortal.CameraState.CAMERA_DEVICE_CLOSED)
-                Thread.sleep(5)
-        }
-
-        val hardwareMap =
-            OpModeManagerImpl.getOpModeManagerOfActivity(AppUtil.getInstance().activity).hardwareMap
-
-        _aprilProcessor =
-            AprilTagProcessor.Builder()
-                .setDrawAxes(true)
-                .setDrawTagID(true)
-                .setDrawTagOutline(true)
-                .setDrawCubeProjection(true)
-                .setDrawTagID(true)
-                .setNumThreads(4)
-                .setOutputUnits(DistanceUnit.METER, AngleUnit.RADIANS)
-                .build()
-
-        val dashboardCameraProcessor = DashboardCameraProcessor()
-
-        _visionPortal =
-            VisionPortal.Builder().setCamera(hardwareMap.get("Webcam 1") as WebcamName)
-                .addProcessors(_aprilProcessor, dashboardCameraProcessor).build()
-
-        FtcDashboard.getInstance().startCameraStream(
-            dashboardCameraProcessor,
-            Configs.TELEMETRY.TELEMETRY_UPDATE_HZ.get().toDouble()
-        )
-    }
-
     override fun dispose() {
-
+        _thread.interrupt()
     }
 
     private constructor() {
         if (Configs.CAMERA.CAMERA_ENABLE) {
-            HotRun.LAZY_INSTANCE.opModeInitEvent += {
-                reCreateCamera()
-            }
-
             ThreadedTelemetry.LAZY_INSTANCE.onTelemetrySend += {
-                _visionPortal?.let { it1 ->
-                    it.addData("camera fps", it1.fps.toString())
-                }
+                it.addData("Robot X", "%.3f".format(robotX))
+                it.addData("Robot Z", "%.3f".format(robotZ))
             }
-
-            HotRun.LAZY_INSTANCE.opModeStopEvent += {
-                FtcDashboard.getInstance().stopCameraStream()
-                _visionPortal?.close()
-            }
-
-            ThreadedEventBus.LAZY_INSTANCE.subscribe(CloseCameraEvent::class, {
-                _thread.interrupt()
-
-                FtcDashboard.getInstance().stopCameraStream()
-                _visionPortal?.close()
-            })
         }
     }
-}*/
+}
